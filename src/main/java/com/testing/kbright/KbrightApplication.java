3c8d0d1c5a0d3dcd911eec0880f8427891317ae6
dprogrammer <kpidibadavid1@gmail.comm>
2024-03-19 14:58:11 +0000

    Start Project

---
+++ src/main/java/com/testing/kbright/KbrightApplication.java
@@ -0,0 +1,13 @@
+package com.testing.kbright;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbrightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbrightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testing/kbright/KbrightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testing.kbright;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbrightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

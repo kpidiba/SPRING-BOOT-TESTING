90039c87b0d976ead416e1150ce200ca80eecb77
kbrightcoder <kpidibadavid1@gmail.com>
2024-03-31 17:39:48 +0000

    add Post Test

--- src/main/java/com/testing/kbright/controller/BookController.java
+++ src/main/java/com/testing/kbright/controller/BookController.java
@@ -3,21 +3,17 @@ package com.testing.kbright.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.testing.kbright.models.Book;
-import com.testing.kbright.repository.BookRepository;
 import com.testing.kbright.service.attachement.BookService;
-
 import lombok.AllArgsConstructor;
 import java.util.List;
-
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @RestController
@@ -51,8 +47,9 @@ public class BookController {
      * @return the registered book
      */
     @PostMapping
-    public Book register(@RequestBody Book book) {
-        return this.service.register(book);
+    public ResponseEntity<Void> register(@RequestBody Book book) {
+        this.service.register(book);
+        return ResponseEntity.ok().build();
     }
 
     /**
--- src/test/java/com/testing/kbright/controller/BookControllerTest.java
+++ src/test/java/com/testing/kbright/controller/BookControllerTest.java
@@ -1,5 +1,8 @@
 package com.testing.kbright.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
@@ -8,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -21,53 +25,73 @@ import com.testing.kbright.service.attachement.BookService;
 @WebMvcTest(BookController.class)
 public class BookControllerTest {
 
-    @Autowired
-    private MockMvc mvc;
+  @Autowired
+  private MockMvc mvc;
 
-    @MockBean
-    private BookService bookService;
+  @MockBean
+  private BookService bookService;
 
-    Book bookMock = new Book(2,"Livre 1","Description",3);
-    String exampleBookJson = "{\"id\":2,\"titre\":\"Livre 1\",\"description\":\"Description\",\"rating\":3}";
+  Book bookMock = new Book(2, "Livre 1", "Description", 3);
+  String exampleBookJson = "{\"id\":2,\"titre\":\"Livre 1\",\"description\":\"Description\",\"rating\":3}";
 
-    @Test
-    public void getAllEmployeesAPI() throws Exception {
-        Mockito.when(bookService.findById(Mockito.anyInt())).thenReturn(bookMock);
 
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
-				"/book/2").accept(
-				MediaType.APPLICATION_JSON);
+  @BeforeAll
+  static void launch(){
+    System.out.println("Man with mission");
+  }
 
-		MvcResult result = mvc.perform(requestBuilder).andReturn();
+  @Test
+  public void getAllEmployeesAPI() throws Exception {
+    Mockito.when(bookService.findById(Mockito.anyInt())).thenReturn(bookMock);
 
-		System.out.println(result.getResponse());
-		String expected = "{\"id\":2,\"titre\":\"Livre 1\",\"description\":\"Description\",\"rating\":3}";
+    RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
+        "/book/2").accept(
+            MediaType.APPLICATION_JSON);
 
-		JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
-    }
+    MvcResult result = mvc.perform(requestBuilder).andReturn();
 
-    @Test
-    void testDelete() {
+    String expected = "{\"id\":2,\"titre\":\"Livre 1\",\"description\":\"Description\",\"rating\":3}";
 
-    }
+    JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
+  }
 
-    @Test
-    void testGetBookById() {
+  @Test
+  void testDelete() {
 
-    }
+  }
 
-    @Test
-    void testIndex() {
+  @Test
+  void testGetBookById() {
 
-    }
+  }
 
-    @Test
-    void testRegister() {
+  @Test
+  void testIndex() {
 
-    }
+  }
 
-    @Test
-    void testUpdate() {
+  @Test
+  void testRegister() throws Exception {
 
-    }
+    // studentService.addCourse to respond back with mockCourse
+    Mockito.when(bookService.register(
+        Mockito.any(Book.class))).thenReturn(bookMock);
+
+    // Send course as body to /students/Student1/courses
+    RequestBuilder requestBuilder = MockMvcRequestBuilders
+        .post("/book")
+        .accept(MediaType.APPLICATION_JSON).content(exampleBookJson)
+        .contentType(MediaType.APPLICATION_JSON);
+
+    MvcResult result = mvc.perform(requestBuilder).andReturn();
+
+    MockHttpServletResponse response = result.getResponse();
+
+    assertEquals(200, response.getStatus());
+  }
+
+  @Test
+  void testUpdate() {
+
+  }
 }

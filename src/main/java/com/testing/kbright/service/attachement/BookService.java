386ef0df24887315e6572c9d6ad65d6b195c741c
kbrightcoder <kpidibadavid1@gmail.com>
2024-05-09 10:53:32 +0000

    ajout de commentaire

--- src/main/java/com/testing/kbright/service/attachement/BookService.java
+++ src/main/java/com/testing/kbright/service/attachement/BookService.java
@@ -3,10 +3,49 @@ package com.testing.kbright.service.attachement;
 import com.testing.kbright.models.Book;
 import java.util.List;
 
+/**
+ * Interface for BookService which provides methods for registering, updating,
+ * finding and deleting books.
+ */
 public interface BookService {
+
+    /**
+     * Registers a new book.
+     * 
+     * @param book the book to register
+     * @return the registered book
+     */
     Book register(Book book);
-    Book update(Book book,Integer id);
+
+    /**
+     * Updates a book with the specified id.
+     * 
+     * @param book the updated book
+     * @param id   the id of the book to update
+     * @return the updated book
+     */
+    Book update(Book book, Integer id);
+
+    /**
+     * Finds a book with the specified id.
+     * 
+     * @param id the id of the book to retrieve
+     * @return the book with the specified id
+     */
     Book findById(Integer id);
+
+    /**
+     * Deletes a book with the specified id.
+     * 
+     * @param id the id of the book to delete
+     */
     void deleteById(Integer id);
+
+    /**
+     * Retrieves a list of all books.
+     * 
+     * @return a list of all books
+     */
     List<Book> getAll();
 }
+
--- src/main/java/com/testing/kbright/service/implementation/BookServiceImpl.java
+++ src/main/java/com/testing/kbright/service/implementation/BookServiceImpl.java
@@ -11,30 +11,66 @@ import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
-public class BookServiceImpl implements BookService{
+/**
+ * Implementation of the BookService interface that uses the BookRepository to
+ * interact with the database. Provides methods for registering, updating,
+ * finding
+ * and deleting books.
+ */
+public class BookServiceImpl implements BookService {
     private final BookRepository repository;
+
+    /**
+     * Registers a new book in the database.
+     * 
+     * @param book the book to register
+     * @return the registered book
+     */
     @Override
     public Book register(Book book) {
         return this.repository.save(book);
     }
 
+    /**
+     * Updates a book in the database with the specified id.
+     * 
+     * @param book the updated book
+     * @param id   the id of the book to update
+     * @return the updated book
+     */
     @Override
     public Book update(Book book, Integer id) {
         book.setId(id);
         return this.repository.save(book);
     }
 
+    /**
+     * Finds a book with the specified id in the database.
+     * 
+     * @param id the id of the book to retrieve
+     * @return the book with the specified id
+     */
     @Override
     public Book findById(Integer id) {
         return this.repository.findById(id).get();
     }
 
+    /**
+     * Deletes a book with the specified id from the database.
+     * 
+     * @param id the id of the book to delete
+     */
     @Override
     public void deleteById(Integer id) {
         Book book = this.repository.findById(id).get();
         this.repository.delete(book);
     }
 
+    /**
+     * Retrieves a list of all books from the database.
+     * 
+     * @return a list of all books
+     */
     @Override
     public List<Book> getAll() {
         return this.repository.findAll();
